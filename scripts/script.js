db9b8fb0d04015b84d6f60d7d555c299d3bb89ae
Xander Gardner <xandergardner7@gmail.com>
2020-12-21 16:02:09 -0500

    added contact section and finished structure of other sections

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
-console.log("More to come");
+const videos = document.querySelectorAll("video")
+
+videos.forEach(video => {
+  video.addEventListener("mouseover", function () {
+    this.play()
+  })
+  
+  video.addEventListener("mouseout", function () {
+    this.pause()
+  })
+  
+  video.addEventListener("touchstart", function () {
+    this.play()
+  })
+  
+  video.addEventListener("touchend", function () {
+    this.pause()
+  })
+})
